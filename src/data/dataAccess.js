0e4c78ea8ba287acaf876df2c035de1ca23f9d00
GabOof <oliveirafonseca.gab@gmail.com>
2024-11-23 14:08:24 -0300

    Implementa conexão com MongoDB usando Mongoose

--- src/data/dataAccess.js
+++ src/data/dataAccess.js
@@ -1,3 +1,16 @@
-class DataAccess {}
+const mongoose = require("mongoose");
 
-export default DataAccess;
+const url = "mongodb://localhost:27017/organizacaoEstudos"; // URL do MongoDB local com nome do banco de dados
+
+async function connectDB() {
+  try {
+    await mongoose.connect(url, {});
+
+    console.log("Conectado ao MongoDB com Mongoose");
+  } catch (error) {
+    console.error("Erro de conexão com o MongoDB:", error);
+    throw error;
+  }
+}
+
+module.exports = { connectDB };
