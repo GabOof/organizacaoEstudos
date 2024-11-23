a03309d2f36d8c08c65192c4347fdb5205522fe0
GabOof <oliveirafonseca.gab@gmail.com>
2024-11-23 14:14:26 -0300

    Adiciona comentários explicativos e melhora a legibilidade nas funções de acesso a dados para MongoDB

--- src/data/dataAccess.js
+++ src/data/dataAccess.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/organizacaoEstudos"; // URL do MongoDB local com nome do banco de dados
+// URL de conexão com o MongoDB local, especificando o nome do banco de dados
+const url = "mongodb://localhost:27017/organizacaoEstudos"; 
 
+// Função assíncrona para conectar ao MongoDB
 async function connectDB() {
   try {
+    // Tenta conectar ao MongoDB usando a URL definida
     await mongoose.connect(url, {});
 
+    // Se a conexão for bem-sucedida, exibe uma mensagem de sucesso no console
     console.log("Conectado ao MongoDB com Mongoose");
   } catch (error) {
+    // Se ocorrer um erro na conexão, exibe a mensagem de erro no console
     console.error("Erro de conexão com o MongoDB:", error);
+    // Lança o erro para que ele possa ser tratado em outro lugar, se necessário
     throw error;
   }
 }
 
+// Exporta a função para que possa ser usada em outras partes do código
 module.exports = { connectDB };
