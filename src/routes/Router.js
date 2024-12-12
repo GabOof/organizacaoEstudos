db11d953d5073ad439bb8142832cbd6a12312168
GabriellyVitoria5 <assuncaogabrielly5@gmail.com>
2024-12-12 17:24:55 -0300

    Atualizando rotas

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const MateriaDAO = require("../data/materiaDAO");
 const EstudanteController = require("../controllers/estudanteController");
-const EstudanteDAOMongo = require("../data/EstudanteDAOMongo");
+const MateriaController = require("../controllers/materiaController");
 const CronogramaController = require("../controllers/cronogramaController");
+const EstudanteDAOMongo = require("../data/estudanteDAOs/EstudanteDAOMongo");
+const MateriaDAOMongo = require("../data/materiaDAOs/MateriaDAOMongo");
+const CronogramaDAOMongo = require("../data/cronogramaDAOs/CronogramaDAOMongo");
 
 // Classe para configurar as rotas da aplicação
 class Router {
@@ -24,10 +26,11 @@ class Router {
 
   // Método para configurar as rotas da aplicação
   configureRoutes() {
-    // Instanciando controller do estudante
-    const estudanteController = new EstudanteController(
-      new EstudanteDAOMongo()
-    );
+
+    // Instanciando os controllers de models
+    const estudanteController = new EstudanteController(new EstudanteDAOMongo());
+    const materiaController = new MateriaController(new MateriaDAOMongo());
+    const cronogramaController = new CronogramaController(new CronogramaDAOMongo(), new EstudanteDAOMongo(), new MateriaDAOMongo());
 
     // Rota para criar um estudante
     this.app.post(
@@ -35,123 +38,35 @@ class Router {
       async (req, res) => await estudanteController.salvarEstudante(req, res)
     );
 
-    // TODO criar materiaController e chamar um método de salvarMateria do controller. Main não pode chamar DAO direto
     // Rota para criar uma matéria
-    this.app.post("/materia", async (req, res) => {
-      try {
-        // Desestruturação dos dados do corpo da requisição
-        const { nome, prioridade, tempoEstimado, estudanteId } = req.body;
+    this.app.post(
+        "/materia",
+        async (req, res) => await materiaController.salvarMateria(req, res)
+    );
 
-        // Verifica se o estudanteId foi informado
-        if (!estudanteId) {
-          return res.status(400).json({ message: "EstudanteId é obrigatório" });
-        }
-
-        // Chama o DAO para salvar a matéria no banco de dados
-        const materia = await MateriaDAO.salvarMateria({
-          nome,
-          prioridade,
-          tempoEstimado,
-          estudanteId,
-        });
-        res.status(201).json(materia); // Retorna a matéria criada com status 201
-      } catch (error) {
-        res.status(500).json({ error: "Erro ao cadastrar matéria" }); // Retorna erro 500 se falhar ao salvar
-      }
-    });
-
-    // TODO criar um método no estudanteController de buscarEstudantePorNome. Main não pode chamar DAO diretamente
     // Rota para buscar estudante pelo nome
-    this.app.get("/estudante/nome/:nome", async (req, res) => {
-      const { nome } = req.params; // Desestruturação do nome do estudante da URL
-
-      try {
-        const estudanteDAO = new EstudanteDAOMongo(); // Instancia a classe EstudanteDAOMongo
-        // Chama o DAO para buscar o estudante no banco de dados
-        const estudante = await estudanteDAO.buscarEstudantePorNome(nome);
-
-        // Verifica se o estudante foi encontrado
-        if (!estudante) {
-          return res.status(404).json({ message: "Estudante não encontrado" }); // Retorna erro 404 se não encontrar o estudante
-        }
-        res.status(200).json(estudante); // Retorna o estudante encontrado com status 200
-      } catch (error) {
-        console.error("Erro ao buscar estudante:", error); // Log do erro no console
-        res
-          .status(500)
-          .json({ message: "Erro ao buscar estudante", error: error.message }); // Retorna erro 500 se falhar ao buscar o estudante
-      }
-    });
+    this.app.get(
+        "/estudante/nome/:nome",
+        async (req, res) => await estudanteController.buscarEstudantePorNome(req, res)
+    );
 
     // Rota para gerar o cronograma
-    this.app.get("/cronograma/:estudanteNome", async (req, res) => {
-      const { estudanteNome } = req.params; // Desestruturação do nome do estudante da URL
-
-      try {
-        // Chama o controller para gerar o cronograma do estudante
-        const cronograma = await CronogramaController.gerarCronograma(
-          estudanteNome
-        );
-
-        // Verifica se o cronograma foi gerado
-        if (cronograma.length === 0) {
-          return res.status(404).json({
-            message:
-              "Nenhuma matéria ou tempo disponível para gerar o cronograma",
-          }); // Retorna erro 404 se não encontrar matérias ou tempo disponível
-        }
-        res.status(200).json(cronograma); // Retorna o cronograma gerado com status 200
-      } catch (error) {
-        res.status(500).json({ error: "Erro ao gerar cronograma" }); // Retorna erro 500 se falhar ao gerar o cronograma
-      }
-    });
+    this.app.get(
+        "/cronograma/:estudanteNome",
+        async (req, res) => await cronogramaController.gerarCronograma(req, res)
+    );
 
     // Rota para marcar uma matéria como estudada
-    this.router.post("/materia/estudar/:materiaId", async (req, res) => {
-      try {
-        // Recupera o id da matéria da URL
-        const materiaId = req.params.materiaId;
-        const materiaAtualizada =
-          await CronogramaController.marcarMateriaEstudada(materiaId); // Chama o controller para marcar a matéria como estudada
-        res.json(materiaAtualizada); // Retorna a matéria atualizada
-      } catch (error) {
-        res.status(400).json({
-          message: "Erro ao atualizar a matéria",
-          error: error.message,
-        }); // Retorna erro 400 se falhar ao marcar a matéria como estudada
-      }
-    });
+    this.router.post(
+        "/materia/estudar/:materiaId",
+        async (req, res) => await cronogramaController.marcarMateriaEstudada(req, res)
+    );
 
-    // TODO criar materiaController e chamar um método de atualizaMataria do controller. Main não pode chamar DAO direto
-    // Rota para editar uma matéria no cronograma
-    this.app.post("/cronograma/editar/:materiaId", async (req, res) => {
-      const { materiaId } = req.params; // Recupera o id da matéria da URL
-      const { tempoAlocado, prioridade } = req.body; // Desestruturação dos dados do corpo da requisição
-
-      try {
-        const novosDados = {}; // Objeto para armazenar os novos dados da matéria
-
-        // Verifica se o tempoAlocado e prioridade foram informados
-        if (tempoAlocado) novosDados.tempoEstimado = tempoAlocado;
-        if (prioridade) novosDados.prioridade = prioridade;
-
-        // Chama o DAO para atualizar a matéria no banco de dados
-        const materiaAtualizada = MateriaDAO.atualizarMateria(
-          materiaId,
-          novosDados
-        );
-
-        res.json(materiaAtualizada); // Retorna a matéria atualizada
-      } catch (error) {
-        console.error("Erro na edição do cronograma:", error); // Log do erro no console
-        if (error.message.includes("Matéria não encontrada")) {
-          return res.status(404).json({ message: "Matéria não encontrada" }); // Retorna erro 404 se a matéria não for encontrada
-        }
-        res
-          .status(500)
-          .json({ message: "Erro ao editar matéria no cronograma" }); // Retorna erro 500 se falhar ao editar a matéria
-      }
-    });
+    // Rota para marcar uma matéria como estudada
+    this.app.post(
+        "/materia/editar/:materiaId",
+        async (req, res) => await materiaController.atualizarMateria(req, res)
+    );
 
     // Middleware para tratamento de erros
     this.app.use(this.router);
